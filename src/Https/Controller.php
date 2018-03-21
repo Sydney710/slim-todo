de3d7951eff1b69eb7f4af7a585e50ecdd1219a0
Sugar <hxtgirq710@qq.com>
2018-03-21 09:30:33 +0800

    add method jsonRes

--- src/Https/Controller.php
+++ src/Https/Controller.php
@@ -12,4 +12,25 @@ class Controller
     {
         $this->container = $container;
     }
+
+    /**
+     * JSON 数据返回
+     *
+     * @param int $status
+     * @param string $info
+     * @param array $playload
+     * @return mixed
+     */
+    protected function jsonRes($status = 1, $info = 'OK', $playload = [])
+    {
+        $data = [
+            "status" => $status,
+            "info" => $info,
+        ];
+        if (empty($playload)) {
+            $data["data"] = $playload;
+        }
+        return $this->container->response->withJson($data, 200, JSON_UNESCAPED_UNICODE);
+    }
+
 }
