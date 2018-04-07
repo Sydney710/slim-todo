666745653c781c115e3a7fe6854ad03b58497ad6
Sydney710 <hxtgirq710@qq.com>
2018-04-07 21:15:46 +0800

    fix token exception

--- src/Middleware/JwtMiddleware.php
+++ src/Middleware/JwtMiddleware.php
@@ -133,15 +133,21 @@ class JwtMiddleware
      * 获取Token值
      *
      * @return array
+     * @throws TodoException
      */
     private function getJwt()
     {
         $token = $this->req->getHeaderLine("X-Token");
         $key = $this->container['settings']['jwt']['key'];
         if (empty($token)) {
-            throw new \InvalidArgumentException("Token 不能为空");
+            throw new \InvalidArgumentException("Token 不能为空", -1);
         }
-        return (array)JWT::decode($token, $key, ['HS256']);
+        try {
+            $jwt = (array)JWT::decode($token, $key, ['HS256']);
+        } catch (\Exception $e) {
+            throw new TodoException("Token 异常", -1);
+        }
+        return $jwt;
     }
 
     /**
@@ -150,22 +156,33 @@ class JwtMiddleware
      */
     private function verifyToken()
     {
-        $now = time();
         $jwt = $this->getJwt();
-        if ($now > $jwt["exp"] && !in_array($this->path, $this->refresh_path)) {
-            throw new TodoException("Token 已过期", -1);
+        if (in_array($this->path, $this->refresh_path)) {
+            return true;
         }
-        if (empty($jwt["uid"])) {
+        $account = $this->getAccount($jwt["uid"]);
+        $this->req = $this->req->withAttribute("jwt", $account);
+        return true;
+    }
+
+    /**
+     * @param $uid
+     * @return mixed
+     * @throws TodoException
+     */
+    private function getAccount($uid)
+    {
+        if (empty($uid)) {
             throw new TodoException("Token 错误", -1);
         }
-        $account = Account::find(hash2id($jwt["uid"]));
+        $account = Account::find(hash2id($uid));
         if (empty($account)) {
             throw new TodoException("Token 错误", -1);
         }
         if ($account->is_lock == 'T') {
             throw new TodoException("账号已锁定", 0);
         }
-        $this->req = $this->req->withAttribute("jwt", $account);
-        return true;
+        return $account;
     }
+
 }
