a97c9dd99214ff037a82a1bbc844a7f7b9493cc4
Sydney710 <hxtgirq710@qq.com>
2018-04-07 20:51:20 +0800

    JWT 验证中间件

--- src/middleware.php
+++ src/middleware.php
@@ -1,1 +1,7 @@
 <?php
+
+$app->add(new \App\Middleware\JwtMiddleware($app->getContainer(), [
+    'paths' => ['/v1.0'],
+    'through' => ['/v1.0/init', '/v1.0/auth/token'],
+    'refresh' => ["/v1.0/auth/refresh"],
+]));
